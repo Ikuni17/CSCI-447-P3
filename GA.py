13d2f910d73ec8e7f1ff32aa4c507e5a1faa3c77
trent <b.trent5@gmail.com>
2017-11-06 19:00:31 -0700

    2 point crossover

--- GA.py
+++ GA.py
@@ -11,11 +11,10 @@ import random
 
 crossover_rate = 1
 mutation_rate = 0.1
-num_slice_points = 3
 evaluation = []
 
-parent_1 = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-parent_2 = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+parent_1 = [] #[0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+parent_2 = [] #[1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
 
 def init_population(size):
 	population = []
@@ -29,26 +28,36 @@ def evaluate():
 
 def crossover(p1, p2):
 	global crossover_rate
-	global num_slice_points
+	offspring_1 = []
+	offspring_2 = []
 
-	pieces = [[]]
-	# need to make sure these one is greater than the other
-	point_1 = random.randrange(len(parent_1))
-	point_2 = random.randrange(len(parent_2))
+	# for testing
+	for i in range(5):
+		parent_1.append(0)
+		parent_2.append(1)
 
 	if random.random() < crossover_rate:
 		# crossover occurs
 		print('Crossover occured')
-		if num_slice_points > len(parent_1) - 1:
-			print('invalid num_slice_points')
-		else:
-			for i in range(num_slice_points):
-				# appends the first chunk to pieces
 
-		print(str(pieces))
+		# select crossover points
+		point_1 = random.randrange(0, len(parent_1))
+		point_2 = random.randrange(point_1, len(parent_1))
+
+		offspring_1.append(parent_1[:point_1])
+		offspring_1.append(parent_2[point_1:point_2])
+		offspring_1.append(parent_1[point_2:])
+
+		print('Points: ' + str(point_1) + ', ' + str(point_2))
+
+
+		print(str(flatten(offspring_1)))
 	else:
 		# crossover does not occur
 		print('Crossover did not occur')
+
+def flatten(input):
+	return [item for sublist in input for item in sublist]
 
 def mutate(child):
 	global mutation_rate
