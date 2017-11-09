5560e712129ea27d9658e5a0d2d92a7418264d48
Brad <white.brad17@gmail.com>
2017-11-09 08:48:55 -0700

    Syntax errors and formatting

--- GA.py
+++ GA.py
@@ -13,184 +13,193 @@ crossover_rate = .5
 mutation_rate = .1
 evaluation = []
 
-p1 = [] #[0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-p2 = [] #[1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+p1 = []  # [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+p2 = []  # [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+
 
 def init_population(size):
-	population = []
-	weights = MLP.get_nn()
-	for i in range(size):
-		population.append(weights)
-	return population
+    population = []
+    weights = MLP.get_nn()
+    for i in range(size):
+        population.append(weights)
+    return population
+
 
 def evaluate(individual):
-	return sum(individual)
+    return sum(individual)
+
 
 def crossover_multipoint(parents, num_children):
-	'''takes a list of parents and produces (num_children) children with a random number of randomly selected slice points '''
+    '''takes a list of parents and produces (num_children) children with a random number of randomly selected slice points '''
 
-	global crossover_rate
+    global crossover_rate
 
-	children = []
-	parent_num = 0
+    children = []
+    parent_num = 0
 
-	# generate num_children children
-	for index in range(num_children):
-		children.append([])
-		# decide each attribute for the current child
-		for attribute in range(len(parents[0])):
-			if random.random() < crossover_rate:
-				# randomly select a parent from parents
-				parent_num = int(random.random() * len(parents))
-			children[index].append(parents[parent_num][attribute])
-	return children
-
-
+    # generate num_children children
+    for index in range(num_children):
+        children.append([])
+        # decide each attribute for the current child
+        for attribute in range(len(parents[0])):
+            if random.random() < crossover_rate:
+                # randomly select a parent from parents
+                parent_num = int(random.random() * len(parents))
+            children[index].append(parents[parent_num][attribute])
+    return children
 
 
 def crossover_2point(parent_1, parent_2):
-	''' takes two parents and produces two offspring with 2 randomly selected slice points '''
+    ''' takes two parents and produces two offspring with 2 randomly selected slice points '''
 
-	global crossover_rate
-	offspring_1 = []
-	offspring_2 = []
+    global crossover_rate
+    offspring_1 = []
+    offspring_2 = []
 
-	if random.random() < crossover_rate:
-		# crossover occurs
-		print('Crossover occured')
+    if random.random() < crossover_rate:
+        # crossover occurs
+        print('Crossover occured')
 
-		# select crossover points
-		point_1 = random.randrange(0, len(parent_1))
-		point_2 = random.randrange(point_1 + 1, len(parent_1))
+        # select crossover points
+        point_1 = random.randrange(0, len(parent_1))
+        point_2 = random.randrange(point_1 + 1, len(parent_1))
 
-		offspring_1.append(parent_1[:point_1])
-		offspring_1.append(parent_2[point_1:point_2])
-		offspring_1.append(parent_1[point_2:])
+        offspring_1.append(parent_1[:point_1])
+        offspring_1.append(parent_2[point_1:point_2])
+        offspring_1.append(parent_1[point_2:])
 
-		offspring_2.append(parent_2[:point_1])
-		offspring_2.append(parent_1[point_1:point_2])
-		offspring_2.append(parent_2[point_2:])
+        offspring_2.append(parent_2[:point_1])
+        offspring_2.append(parent_1[point_1:point_2])
+        offspring_2.append(parent_2[point_2:])
 
-	else:
-		# crossover does not occur
-		print('Crossover did not occur')
+    else:
+        # crossover does not occur
+        print('Crossover did not occur')
+
 
 def flatten(input):
-	''' flattens a ragged 2-d array into a 1-d array '''
-	return [item for sublist in input for item in sublist]
+    ''' flattens a ragged 2-d array into a 1-d array '''
+    return [item for sublist in input for item in sublist]
+
 
 def mutate(child):
-	''' has a (mutation_rate) chance to change each attribute randomly by up to +/- 50% '''
+    ''' has a (mutation_rate) chance to change each attribute randomly by up to +/- 50% '''
 
-	global mutation_rate
+    global mutation_rate
 
-	for attribute in range(len(child)):
-		if random.random() < mutation_rate:
-			# mutates an attribute by at most +/- 50%
-			if child[attribute] == 0:
-				child[attribute] += (sum(child) / len(child))
-			else:
-				child[attribute] += (random.random() - 0.5) * child[attribute]
-		else:
-			pass
-			# print('mutation did not occur')
-	return child
+    for attribute in range(len(child)):
+        if random.random() < mutation_rate:
+            # mutates an attribute by at most +/- 50%
+            if child[attribute] == 0:
+                child[attribute] += (sum(child) / len(child))
+            else:
+                child[attribute] += (random.random() - 0.5) * child[attribute]
+        else:
+            pass
+        # print('mutation did not occur')
+    return child
+
 
 def selection(population):
-	pass
+    pass
+
 
 def tournament_selection(population, heat_size):
-	# UNTESTED BECAUSE WE DONT HAVE EVALUATE
-	''' selects (num_select) individuals from (population) and holds a tournament with (heat_size) heats '''
+    # UNTESTED BECAUSE WE DONT HAVE EVALUATE
+    ''' selects (num_select) individuals from (population) and holds a tournament with (heat_size) heats '''
 
-	num_select = len(population)
-	selected = []
+    num_select = len(population)
+    selected = []
 
-	# to select num_select individuals
-	for i in range(num_select):
-		# randomly select heat_size individuals from the population
-		heat = []
-		for individual in range(heat_size):
-			# add a random individual to heat
+    # to select num_select individuals
+    for i in range(num_select):
+        # randomly select heat_size individuals from the population
+        heat = []
+        for individual in range(heat_size):
+            # add a random individual to heat
 
-			heat.append(population[int(random.random() * len(population))])
+            heat.append(population[int(random.random() * len(population))])
 
-		# find the best individual from heat and add it to selected
-		# ASSUMING MINIMIZATION
-		if heat != []:
-			min = evaluate(heat[0])
-			min_index = 0
+        # find the best individual from heat and add it to selected
+        # ASSUMING MINIMIZATION
+        if heat != []:
+            min = evaluate(heat[0])
+            min_index = 0
 
-			for contestant in heat:
-				temp_fitness = evaluate(contestant)
-				if temp_fitness < min:
-					min = temp_fitness
-					min_index = heat.index(contestant)
+            for contestant in heat:
+                temp_fitness = evaluate(contestant)
+                if temp_fitness < min:
+                    min = temp_fitness
+                    min_index = heat.index(contestant)
 
-			selected.append(heat[min_index])
-	return selected
+            selected.append(heat[min_index])
+    return selected
+
 
 def train():
-	generation = 0
+    generation = 0
+
 
 def test_cross_mutate():
-	print('parameters')
-	print('crossover_rate: ' + str(crossover_rate))
-	print('mutation_rate: ' + str(mutation_rate))
+    print('parameters')
+    print('crossover_rate: ' + str(crossover_rate))
+    print('mutation_rate: ' + str(mutation_rate))
 
-	p1 = []
-	p2 = []
-	for i in range(10):
-		p1.append(0)
-		p2.append(1)
+    p1 = []
+    p2 = []
+    for i in range(10):
+        p1.append(0)
+        p2.append(1)
 
-	parents = [p1, p2]
+    parents = [p1, p2]
 
-	print('\nparents')
-	for parent in parents:
-		print(str(parent))
+    print('\nparents')
+    for parent in parents:
+        print(str(parent))
 
-	children = crossover_multipoint([p1, p2], 10)
-	print('\nchildren')
-	for individual in children:
-		print(str(individual))
+    children = crossover_multipoint([p1, p2], 10)
+    print('\nchildren')
+    for individual in children:
+        print(str(individual))
 
-	for individual in children:
-		individual = mutate(individual)
+    for individual in children:
+        individual = mutate(individual)
 
-	print('\nmutated children')
-	for individual in children:
-		print(str(individual))
+    print('\nmutated children')
+    for individual in children:
+        print(str(individual))
+
 
 def generate_random_individual(length):
-	individual = []
-	for i in range(length):
-		individual.append(random.random())
-	return individual
+    individual = []
+    for i in range(length):
+        individual.append(random.random())
+    return individual
+
 
 def test_select():
-	population_size = 50
-	heat_size = 5
-	length = 4
+    population_size = 50
+    heat_size = 5
+    length = 4
 
-	population = []
+    population = []
 
-	for i in range(population_size):
-		population.append(generate_random_individual(length))
+    for i in range(population_size):
+        population.append(generate_random_individual(length))
 
-	print('\ntotal')
-	original_sum = 0
-	sum = 0
-	for i in population:
-		original_sum += i[0]
+    print('\ntotal')
+    original_sum = 0
+    sum = 0
+    for i in population:
+        original_sum += i[0]
 
-	population = tournament_selection(population, heat_size)
+    population = tournament_selection(population, heat_size)
 
-	for i in population:
-		sum += i[0]
+    for i in population:
+        sum += i[0]
 
-	print(str(original_sum) + '\n' + str(sum))
+    print(str(original_sum) + '\n' + str(sum))
 
 
 if __name__ == '__main__':
-	test_select()
+    test_select()
--- MLP.py
+++ MLP.py
@@ -4,117 +4,115 @@ import rosen_generator as rosen
 
 
 class MLP2:
-	def __init__(self, num_inputs, num_hidden_layers, nodes_per_layer, training_data, learning_rate=0.1,
-				 iterations=1000):
-		self.weights = []  # Each numpy array in this represents the weights coming into a node
-		self.inputs = []
-		self.train_in = []
-		self.train_out = []
-		self.activation = []  # Each numpy array in this represents the activation leaving a node for every input
-		self.iterations = iterations
-		self.learning_rate = learning_rate
-		for x in training_data:
-			self.train_in.append(x[:num_inputs])
-			self.train_out.append(x[num_inputs:])
-		print('Setting up the network with {0} inputs and {1} output(s)'.format(len(self.train_in[0]),
-																				len(self.train_out[0])))
-		# print('Train_in: {0}'.format(train_in))
-		# Initilize the NN with random weights and populate the activation matrix
-		for i in range(num_hidden_layers + 1):
-			self.weights.append([])  # append a matrix to represent a layer in the NN
-			if i == 0:
-				# this transposes the input to the right format and adds as the first activation layer
-				self.activation.append(np.array(self.train_in).transpose())
-				num_nodes = nodes_per_layer
-				num_weights = num_inputs
-			# use the number of outputs for the last layer
-			elif i == num_hidden_layers:
-				num_weights = num_nodes
-				num_nodes = len(self.train_out[0])
-				self.activation.append(np.array([]))
-			# Otherwise it's a hidden_layer
-			else:
-				num_weights = num_nodes
-				num_nodes = nodes_per_layer
-				self.activation.append(np.array([]))
+    def __init__(self, num_inputs, num_hidden_layers, nodes_per_layer, training_data, learning_rate=0.1,
+                 iterations=1000):
+        self.weights = []  # Each numpy array in this represents the weights coming into a node
+        self.inputs = []
+        self.train_in = []
+        self.train_out = []
+        self.activation = []  # Each numpy array in this represents the activation leaving a node for every input
+        self.iterations = iterations
+        self.learning_rate = learning_rate
+        for x in training_data:
+            self.train_in.append(x[:num_inputs])
+            self.train_out.append(x[num_inputs:])
+        print('Setting up the network with {0} inputs and {1} output(s)'.format(len(self.train_in[0]),
+                                                                                len(self.train_out[0])))
+        # print('Train_in: {0}'.format(train_in))
+        # Initilize the NN with random weights and populate the activation matrix
+        for i in range(num_hidden_layers + 1):
+            self.weights.append([])  # append a matrix to represent a layer in the NN
+            if i == 0:
+                # this transposes the input to the right format and adds as the first activation layer
+                self.activation.append(np.array(self.train_in).transpose())
+                num_nodes = nodes_per_layer
+                num_weights = num_inputs
+            # use the number of outputs for the last layer
+            elif i == num_hidden_layers:
+                num_weights = num_nodes
+                num_nodes = len(self.train_out[0])
+                self.activation.append(np.array([]))
+            # Otherwise it's a hidden_layer
+            else:
+                num_weights = num_nodes
+                num_nodes = nodes_per_layer
+                self.activation.append(np.array([]))
 
-			# Each matrix in a layer represents a node and holds all the weights coming into the node
-			for j in range(num_nodes):
-				temp = []
-				for k in range(num_weights):
-					temp.append(random.uniform(0, 10))
-				self.weights[i].append(np.array(temp))
-		self.activation.append(np.array([]))
+            # Each matrix in a layer represents a node and holds all the weights coming into the node
+            for j in range(num_nodes):
+                temp = []
+                for k in range(num_weights):
+                    temp.append(random.uniform(0, 10))
+                self.weights[i].append(np.array(temp))
+        self.activation.append(np.array([]))
 
-	def swap_weights(weights):
-		# Layers
-		for i in range(len(self.weights)):
-			# Nodes with weight arrays
-			for j in range(len(self.weights[i])):
-				self.weights[i][j] = weights[:self.actiaviton[i]]
-		self.feedforward
+    def swap_weights(self, weights):
+        # Layers
+        for i in range(len(self.weights)):
+            # Nodes with weight arrays
+            for j in range(len(self.weights[i])):
+                self.weights[i][j] = weights[:self.activation[i]]
+        self.feedforward()
 
+    def calc_avg_error(self):
+        return np.average(np.array(self.train_out).transpose() - self.activation[len(self.activation) - 1])
 
-	def calc_avg_error():
-		return np.average(np.array(self.train_out).transpose() - self.activation[len(self.activation) - 1])
+    def train(self):
+        for i in range(self.iterations):
+            self.feedforward()
+            error = self.backprop()
+            if i % 100 == 0:
+                print('Error at iteration {0}: {1}'.format(i, error))
 
+    # updates the activation arrays and the output
+    def feedforward(self):
+        for i in range(len(self.weights)):
+            temp = np.zeros((len(self.weights[i]), len(self.activation[0][0])))
+            for j in range(len(self.weights[i])):
+                # append all the activations to list to be converted to an np array as an actiaviton layer
+                temp[j] = self.activation[i].transpose().dot(self.weights[i][j])
+            # don't run the activation on the tanh function
+            if (i == len(self.weights) - 1):
+                self.activation[i + 1] = np.array(temp)
+            else:
 
-	def train(self):
-		for i in range(self.iterations):
-			self.feedforward()
-			error = self.backprop()
-			if i % 100 == 0:
-				print('Error at iteration {0}: {1}'.format(i, error))
+                self.activation[i + 1] = np.tanh(temp)
 
-	# updates the activation arrays and the output
-	def feedforward(self):
-		for i in range(len(self.weights)):
-			temp = np.zeros((len(self.weights[i]), len(self.activation[0][0])))
-			for j in range(len(self.weights[i])):
-				# append all the activations to list to be converted to an np array as an actiaviton layer
-				temp[j] = self.activation[i].transpose().dot(self.weights[i][j])
-			# don't run the activation on the tanh function
-			if (i == len(self.weights) - 1):
-				self.activation[i + 1] = np.array(temp)
-			else:
+    def backprop(self):
+        # print('Expected: {0}, Actual: {1}'.format(self.train_out[0], self.activation[len(self.activation)-1]))
+        error = np.average(np.array(self.train_out).transpose() - self.activation[len(self.activation) - 1])
+        for i, layer in reversed(list(enumerate(self.weights))):
+            for j in range(len(layer)):
+                activ_out = self.activation[i + 1][j]
+                modifiers = self.learning_rate * activ_out * error
+                self.weights[i][j] = np.add(self.weights[i][j], np.sum(modifiers))
+        return error
 
-				self.activation[i + 1] = np.tanh(temp)
+    def print_nn(self):
+        print('Dimensions of weights \n --------------------------')
+        for layers in self.weights:
+            for weight in layers:
+                print(weight.shape, end='')
+            print('\n')
+        print('Dimensions of activations \n --------------------------')
+        for layer in self.activation:
+            print(layer.shape)
 
-	def backprop(self):
-		# print('Expected: {0}, Actual: {1}'.format(self.train_out[0], self.activation[len(self.activation)-1]))
-		error = np.average(np.array(self.train_out).transpose() - self.activation[len(self.activation) - 1])
-		for i, layer in reversed(list(enumerate(self.weights))):
-			for j in range(len(layer)):
-				activ_out = self.activation[i + 1][j]
-				modifiers = self.learning_rate * activ_out * error
-				self.weights[i][j] = np.add(self.weights[i][j], np.sum(modifiers))
-		return error
-
-	def print_nn(self):
-		print('Dimensions of weights \n --------------------------')
-		for layers in self.weights:
-			for weight in layers:
-				print(weight.shape, end='')
-			print('\n')
-		print('Dimensions of activations \n --------------------------')
-		for layer in self.activation:
-			print(layer.shape)
-
-	# Calcualte the acitvations of a node given it's wieghts and the values coming into the node
-	@staticmethod
-	def activ_fun(activ, weights):
-		x = np.array(activ).transpose()
-		y = np.array(weights)
-		z = np.tanh(y.dot(x))
-		return z
+    # Calcualte the acitvations of a node given it's wieghts and the values coming into the node
+    @staticmethod
+    def activ_fun(activ, weights):
+        x = np.array(activ).transpose()
+        y = np.array(weights)
+        z = np.tanh(y.dot(x))
+        return z
 
 
 def main():
-	num_inputs = 2
-	training_data = rosen.generate(0, num_inputs)
-	MLP = MLP2(num_inputs, 2, 3, training_data)
-	MLP.train()
+    num_inputs = 2
+    training_data = rosen.generate(0, num_inputs)
+    MLP = MLP2(num_inputs, 2, 3, training_data)
+    MLP.train()
 
 
 if __name__ == "__main__":
-	main()
+    main()
