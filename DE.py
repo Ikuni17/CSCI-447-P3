46208035d6e750f417bc1c5b93d3d5c7cc2b94b7
Logan Bonney <wakeup2early@gmail.com>
2017-11-09 14:43:48 -0700

    wrote train and selection for DE. Still need to test.

--- DE.py
+++ DE.py
@@ -12,7 +12,7 @@ def mutate(population):
 		trail_vectors.append(population[i]+[x * BETA for x in (diff1 - diff2)])
 	return trial_vectors
 
-def crossover(trial_vector, parent):
+def crossover(self, trial_vector, parent):
 	# Generate list of potential corssover points
 	crossover_points = range(0, len(parents)0-1)
 	random.shuffle(crossover_points)
@@ -25,8 +25,28 @@ def crossover(trial_vector, parent):
 	for j in range(len(parent)):
 		if random.uniform() < crossover_prob and j != point:
 			child[j] = trail_vector[crossover_points[j]]
+	return child
 
+def selection(self, child, parent):
+	child_perf = GA,evaluate(child)
+	parent_perf = GA,evaluate(parent)
+	# Choose the best between parents adn children
+	if child_perf > parent_perf:
+		return child
+	else:
+		return parent
 
-
-
+def train(self):
+	generation = 0
+	max_gen = 2000
+	pop_size = 100
+	population = GA.init_population(pop_size) 
+	while generation < max_gen:
+		# Generate trial vectors
+		trial_vectors = mutatate(population)
+		# generate children from trial_vectors
+		for i in range(len(trial_vectors)):
+			child = crossover(trial_vector[i], population[i])
+			parent[i] = selection(child, population[i])
+		
 
