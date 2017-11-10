88dc48fc2768897458e10a030a81cbd4c609bcd2
Brad <white.brad17@gmail.com>
2017-11-10 08:46:21 -0700

    Fixed updating sigmas

--- ES.py
+++ ES.py
@@ -1,5 +1,7 @@
 import GA
 import random
+import math
+
 
 def add_sigmas(pop_size, individual_size):
     sigmas = []
@@ -12,11 +14,14 @@ def add_sigmas(pop_size, individual_size):
     return sigmas
 
 
+# p.218 second bullet
 def update_sigmas(sigmas):
-    tau = (1/sqrt(len(sigmas)))
+    tau = (1 / math.sqrt(2 * math.sqrt(len(sigmas[0]))))
+    tau_prime = (1 / math.sqrt(2 * len(sigmas[0])))
 
     for i in range(len(sigmas)):
-        sigmas[i] = sigma[i]**(tau * random.gauss(0, 1))
+        for j in range(len(sigmas[i])):
+            sigmas[i][j] = sigmas[i][j] * math.exp((tau_prime * random.gauss(0, 1)) + (tau * random.gauss(0, 1)))
 
     return sigmas
 
@@ -28,6 +33,7 @@ def train():
     population = GA.init_population(pop_size)
     sigmas = add_sigmas(pop_size, len(population[0]))
     heat_size = 5
+
 
 if __name__ == '__main__':
     train()
